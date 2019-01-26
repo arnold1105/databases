98a6480b6b58d2b5aba242a2adb225f4c56d1a50
arnold1105 <arnold.b.cheung@gmail.com>
2019-01-25 20:47:45 -0800

    Create table in chat database

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,21 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+--   Id int,
+--   User varchar(500),
+--   Text varchar(8000),
+--   Room varchar(500)
+-- );
 
-/* Create other tables and define schemas for them here! */
-
+-- /* Create other tables and define schemas for them here! */
+-- CREATE TABLE Rooms (
+--   /* Describe your table here.*/
+--   Id int,
+--   Name varchar(500),
+--   MessageIdFk int
+-- );
 
 
 
